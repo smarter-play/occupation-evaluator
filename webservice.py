c566700bac241a4bc896a5c9fca6672c3637947e
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-02-05 17:14:46 +0100

    Add occupation webservice

--- webservice.py
+++ webservice.py
@@ -1,15 +1,43 @@
 from flask import Flask, request
+from occupation import evaluate_occupation
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 
-@app.route("/api/occupation")
+@app.route("/api/occupation", methods=['GET'])
 def occupation():
+    if not 'basket' in request.args:
+        return { 'error': "Missing required field: \"basket\"", }, 400
+
+    if not 't' in request.args:
+        return { 'error': "Missing required field: \"t\"", }, 400
+
+    basket = int(request.args['basket'])
+    t = int(request.args['t'])  # Unix timestamp
+
+    o_t = evaluate_occupation(basket, t)
+    return {
+        'occupation': o_t,
+        'datetime': datetime.fromtimestamp(t)
+    }, 200
+
+
+@app.route("/api/occupation_forecast", methods=["GET"])
+def occupation_forecast():
+    if not 'basket' in request.args:
+        return { 'error': "Missing required field: \"basket\"", }, 400
+
+    if not 't' in request.args:
+        return { 'error': "Missing required field: \"t\"", }, 400
+        
     basket = request.args['basket']
-    t = request.args['t']
-    pass
+    t = request.args['t']  # Unix timestamp
+
+    o_t = 0
+    return { 'occupation': o_t }, 200
 
 
 if __name__ == '__main__':
-    pass
+    app.run()
