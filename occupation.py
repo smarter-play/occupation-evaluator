b92843c3ff0690b7bf8a5822200dbb5c586d2b61
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-02-06 16:49:58 +0100

    Remove usage of Unix timestamp

--- occupation.py
+++ occupation.py
@@ -4,39 +4,51 @@ from math import *
 
 
 def evaluate_occupation(basket_id: int, t):
+    """
+    Evaluate the occupation probability for a given instant or array of instants `t` based on the measurements stored for the given `basket_id`.
+
+    Parameters:
+      - `basket_id`: The basket to evaluate the occupation for
+      - `t`: The list of instants in time, expressed as a python `datetime` (in UTC timezone) or as an array of `datetime(s)`,
+            for which the occupation should be evaluated
+
+    Returns:
+        A single or an array of occupations depending if the input was a scalar or an array
+    """
+
     if np.isscalar(t):
         t = [t]
 
-    t_min, t_max = np.amin(t).item(), np.amax(t).item()
+    t_min, t_max = np.amin(t), np.amax(t)
 
     # Query data
     db_cursor = db_connection.cursor()
 
     db_cursor.execute("""
-        SELECT UNIX_TIMESTAMP(timestamp) FROM accelerometer_data
+        SELECT timestamp FROM accelerometer_data
         WHERE
             basket_id = %s AND
-            timestamp BETWEEN FROM_UNIXTIME(%s) - INTERVAL 30 MINUTE AND FROM_UNIXTIME(%s)
+            timestamp BETWEEN %s - INTERVAL 30 MINUTE AND %s
         ORDER BY
             timestamp ASC
     """, (basket_id, t_min, t_max,))
     accelerometer_data = [row[0] for row in db_cursor.fetchall()]
 
     db_cursor.execute("""
-        SELECT UNIX_TIMESTAMP(timestamp) FROM basket_data
+        SELECT timestamp FROM basket_data
         WHERE
             basket_id = %s AND
-            timestamp BETWEEN FROM_UNIXTIME(%s) - INTERVAL 30 MINUTE AND FROM_UNIXTIME(%s)
+            timestamp BETWEEN %s - INTERVAL 30 MINUTE AND %s
         ORDER BY
             timestamp ASC
     """, (basket_id, t_min, t_max,))
     basket_data = [row[0] for row in db_cursor.fetchall()]
 
     db_cursor.execute("""
-        SELECT UNIX_TIMESTAMP(timestamp) FROM people_detected_data
+        SELECT timestamp FROM people_detected_data
         WHERE
             basket_id = %s AND
-            timestamp BETWEEN FROM_UNIXTIME(%s) - INTERVAL 30 MINUTE AND FROM_UNIXTIME(%s)
+            timestamp BETWEEN %s - INTERVAL 30 MINUTE AND %s
         ORDER BY
             timestamp ASC
     """, (basket_id, t_min, t_max,))
@@ -55,24 +67,19 @@ def evaluate_occupation(basket_id: int, t):
 
             while True:
                 if (base_idx + offset) >= len(measurement_data):
-                    #print(f"base_idx {base_idx} + offset {offset} over {len(measurement_data)}")
                     break
 
                 measure_t = measurement_data[base_idx + offset]
                 if measure_t > t:
                     # The current measure is more recent, we go on with the next time sample
-                    #print(f"measure {datetime.fromtimestamp(measure_t)} > t {datetime.fromtimestamp(t)}")
                     break
 
                 t_delta = t - measure_t
-                #print(f"measure {datetime.fromtimestamp(measure_t)} - t {datetime.fromtimestamp(t)} => t_delta {t_delta} ", end='')
-                if t_delta <= (60 * 30):
-                    # If it's within the 5 minutes range, then it could contribute to the final occupation
-                    #print(f"contrib")
+                if t_delta.total_seconds() <= (60 * 30):
+                    # If it's within the 30 minutes range, then it could contribute to the final occupation
                     occupation_array[i] += contribution(t, measure_t)
                     offset += 1
                 else:
-                    #print(f"going on")
                     base_idx += 1
                     offset = 0
 
@@ -81,7 +88,7 @@ def evaluate_occupation(basket_id: int, t):
         b = 0
         c = p
 
-        x = t - t0
+        x = (t - t0).total_seconds()
         y = max(a * (x ** 2) + b * x + c, 0)
 
         return y
