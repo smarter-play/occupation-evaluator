12225698a1a3f6f9f1a0c1ed39cd14bbbe8b6daf
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-02-06 19:35:37 +0100

    Add occupation_forecast_visualizer

---
+++ occupation_forecast_visualizer.py
@@ -0,0 +1,27 @@
+from occupation_forecast import forecast_occupation
+from datetime import datetime
+
+
+def main():
+    MOCK_BASKET_ID = 0x23232323
+
+    present = datetime(2016, 8, 1)
+    num_history_days = 100
+    num_predicted_days = 14
+    t = datetime(2016, 8, 6, 17, 0, 0)
+
+    occupation = forecast_occupation(
+        MOCK_BASKET_ID,
+        present,
+        num_history_days,
+        num_predicted_days,
+        t,
+        debug=True
+    )
+
+    print(f"Occupation at {t} is: {occupation:.2f}")
+
+
+if __name__ == "__main__":
+    main()
+
